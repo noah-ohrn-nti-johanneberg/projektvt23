38c818d1994f407dd606056edf125f3857528c17
noah-ohrn-nti-johanneberg <118253292+noah-ohrn-nti-johanneberg@users.noreply.github.com>
2023-03-07 09:59:03 +0100

    Revert "tried with hamburger failed go back"
    This reverts commit c2b6d0b54ab121b70f4371954419bf62b9181811.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
 // Denna fil skall innehålla JavaScript (JS)
-
-const hamburger = document.querySelector("#hamburger");
-const nav = document.querySelector("nav");
-
-hamburger.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    nav.classList.toggle("active");
-}
-
-
