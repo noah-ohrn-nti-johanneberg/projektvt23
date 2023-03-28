4fbebd403bb93a2e85ffa92d95687097ce54d5fb
noah-ohrn-nti-johanneberg <118253292+noah-ohrn-nti-johanneberg@users.noreply.github.com>
2023-03-28 10:39:36 +0200

    fixad hamburgare
    fixa runt med head och nav samt fiaxat till hemburger

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Denna fil skall innehålla JavaScript (JS)
+
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector("nav");
+
+hamburger.addEventListener('click', togglemenu);
+
+function togglemenu() {
+    nav.classList.toggle("active");
+}
